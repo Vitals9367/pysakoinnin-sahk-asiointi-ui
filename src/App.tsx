a97f3d33c89fdaeecb32cdd766faa65d66f65ad4
NikoHelle <niko.helle@hyperactive.fi>
2021-05-17 11:29:56 +0300

    HP-702 Separated routes and subpages for each config
    Also Header.tsx and Index.tsx require config type to alter content

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 
-import Index from './pages/Index';
-import ApiAccessTokens from './pages/ApiAccessTokens';
-import Tokens from './pages/Tokens';
-import ProfilePage from './pages/ProfilePage';
 import { ClientProvider } from './client/ClientProvider';
 import StoreProvider from './client/redux/StoreProvider';
-import Header from './components/Header';
 import PageContainer from './components/PageContainer';
 import HandleCallback from './components/HandleCallback';
 import ConfigChecker from './components/ConfigChecker';
+import config from './config';
+import Index from './pages/Index';
+import Tokens from './pages/Tokens';
+import Header from './components/Header';
+import PlainSuomiFiUserInfo from './pages/PlainSuomiFiUserInfo';
+import ApiAccessTokens from './pages/ApiAccessTokens';
+import ProfilePage from './pages/ProfilePage';
 
 function App(): React.ReactElement {
+  const plainSuomiFiPath = config.plainSuomiFiConfig.path;
+  const mvpPath = config.mvpConfig.path;
   return (
     <ConfigChecker>
       <HandleCallback>
@@ -21,16 +25,19 @@ function App(): React.ReactElement {
             <PageContainer>
               <Header />
               <Switch>
-                <Route path={['/']} exact>
+                <Route path={[plainSuomiFiPath, mvpPath]} exact>
                   <Index />
                 </Route>
-                <Route path={['/apiAccessTokens']} exact>
-                  <ApiAccessTokens />
-                </Route>
-                <Route path={['/userTokens']} exact>
+                <Route path={['/:anyPath/userTokens']} exact>
                   <Tokens />
                 </Route>
-                <Route path={['/profile']} exact>
+                <Route path={[`${plainSuomiFiPath}/userinfo`]} exact>
+                  <PlainSuomiFiUserInfo />
+                </Route>
+                <Route path={[`${mvpPath}/apiAccessTokens`]} exact>
+                  <ApiAccessTokens />
+                </Route>
+                <Route path={[`${mvpPath}/profile`]} exact>
                   <ProfilePage />
                 </Route>
                 <Route path={['/authError']} exact>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,111 @@ import React, { useState } from 'react';
 import { Navigation } from 'hds-react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { useClient } from '../client/hooks';
-import config from '../config';
 import styles from './styles.module.css';
+import { getClientConfig } from '../client';
+import config from '../config';
 
-type Page = 'frontpage' | 'apiAccessTokens' | 'userTokens' | 'profile';
+type Page =
+  | 'frontpage'
+  | 'apiAccessTokens'
+  | 'userTokens'
+  | 'profile'
+  | 'userinfo';
 
 const Header = (): React.ReactElement => {
+  const currentConfig = getClientConfig();
+  const pathPrefix = currentConfig.path;
   const client = useClient();
   const authenticated = client.isAuthenticated();
   const initialized = client.isInitialized();
   const user = client.getUser();
   const history = useHistory();
   const location = useLocation();
+  const path = location.pathname.replace(pathPrefix, '');
   const currentPageFromPath: Page =
-    location.pathname && location.pathname.length > 1
-      ? (location.pathname.substr(1) as Page)
-      : 'frontpage';
+    path && path.length > 1 ? (path.substr(1) as Page) : 'frontpage';
   const [active, setActive] = useState<Page>(currentPageFromPath);
 
   const title = 'Helsinki Profiili Example';
   const userName = user ? `${user.given_name} ${user.family_name}` : '';
+
+  const frontPageLink = (
+    <Navigation.Item
+      active={active === 'frontpage'}
+      label="Etusivu"
+      key="frontpage"
+      tabIndex={0}
+      onClick={(): void => {
+        setActive('frontpage');
+        history.push(pathPrefix);
+      }}
+      data-test-id="header-link-frontpage"
+    />
+  );
+
+  const accessTokenLink = (
+    <Navigation.Item
+      active={active === 'apiAccessTokens'}
+      as="button"
+      label="Hae API access token"
+      key="apiAccessTokens"
+      type="button"
+      onClick={(): void => {
+        setActive('apiAccessTokens');
+        history.push(`${pathPrefix}/apiAccessTokens`);
+      }}
+      data-test-id="header-link-apiAccessTokens"
+    />
+  );
+
+  const userTokenLink = (
+    <Navigation.Item
+      active={active === 'userTokens'}
+      as="button"
+      label="Tokenit"
+      key="userTokens"
+      type="button"
+      onClick={(): void => {
+        setActive('userTokens');
+        history.push(`${pathPrefix}/userTokens`);
+      }}
+    />
+  );
+
+  const profileLink = (
+    <Navigation.Item
+      active={active === 'profile'}
+      as="button"
+      label="Profiili"
+      key="profile"
+      type="button"
+      onClick={(): void => {
+        setActive('profile');
+        history.push(`${pathPrefix}/profile`);
+      }}
+      data-test-id="header-link-profile"
+    />
+  );
+  const userInfoLink = (
+    <Navigation.Item
+      active={active === 'userinfo'}
+      as="button"
+      label="User info"
+      key="userinfo"
+      type="button"
+      onClick={(): void => {
+        setActive('userinfo');
+        history.push(`${pathPrefix}/userinfo`);
+      }}
+      data-test-id="header-link-user-info"
+    />
+  );
+
+  // <Navigation.Row> cannot not handle null/undefined as children.
+  // That is why if..else cannot be used in <Navigation.Row>
+  const links = currentConfig.hasApiTokenSupport
+    ? [frontPageLink, accessTokenLink, userTokenLink, profileLink]
+    : [frontPageLink, userTokenLink, userInfoLink];
 
   return (
     <Navigation
@@ -30,52 +115,11 @@ const Header = (): React.ReactElement => {
       menuToggleAriaLabel="Close menu"
       theme="light"
       title={title}
-      titleUrl="https://hel.fi"
+      titleUrl="/"
       skipTo="#content"
       skipToContentLabel="Skip to main content">
       <Navigation.Row variant="inline">
-        <Navigation.Item
-          active={active === 'frontpage'}
-          label="Etusivu"
-          tabIndex={0}
-          onClick={(): void => {
-            setActive('frontpage');
-            history.push('/');
-          }}
-          data-test-id="header-link-frontpage"
-        />
-        <Navigation.Item
-          active={active === 'apiAccessTokens'}
-          as="button"
-          label="Hae API access token"
-          type="button"
-          onClick={(): void => {
-            setActive('apiAccessTokens');
-            history.push('/apiAccessTokens');
-          }}
-          data-test-id="header-link-apiAccessTokens"
-        />
-        <Navigation.Item
-          active={active === 'userTokens'}
-          as="button"
-          label="Tokenit"
-          type="button"
-          onClick={(): void => {
-            setActive('userTokens');
-            history.push('/userTokens');
-          }}
-        />
-        <Navigation.Item
-          active={active === 'profile'}
-          as="button"
-          label="Profiili"
-          type="button"
-          onClick={(): void => {
-            setActive('profile');
-            history.push('/profile');
-          }}
-          data-test-id="header-link-profile"
-        />
+        {links.map(link => link)}
       </Navigation.Row>
       <Navigation.Actions>
         {initialized && (
